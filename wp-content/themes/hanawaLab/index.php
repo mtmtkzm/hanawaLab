9f97d85ef20d5432377631c5fc87978668fe9812
松本 和馬 <hello@mtmtkzm.com>
2016-08-25 16:47:59 +0900

    initial commit

---
+++ wp-content/themes/hanawaLab/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+	<meta charset="UTF-8">
+	<title>HanawaLab.</title>
+	<link rel="stylesheet" href="/css/style.css"> 
+</head>
+<body>
+	<header>header</header>
+	<main>main</main>
+	<footer>footer</footer>
+	<script src="./js/app.js"></script>
+</body>
+</html>
