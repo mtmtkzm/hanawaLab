c504d94323dfe24b08c4012fee825af83c488c3b
松本 和馬 <hello@mtmtkzm.com>
2016-08-27 02:33:21 +0900

    左上のパスもどきを追加

--- wp-content/themes/hanawaLab/index.php
+++ wp-content/themes/hanawaLab/index.php
@@ -6,15 +6,21 @@
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body <?php body_class(); ?> >
+	
+	<header class="header">
+		<div class="logo-path">
+			<a href="/"><h2><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt=""></h2></a>
+			<a href="/">Home</a>
+		</div>
+	</header>
+	<main class="main"></main>
+	<footer class="footer"></footer>
 
 	<?php if(have_posts()): while(have_posts()): the_post(); ?>
 	<article <?php post_class(); ?>>
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
+		<h3><?php the_title(); ?></h3>
 	</article>
 
 	<?php endwhile; endif; ?>
-
-
 </body>
 </html>
