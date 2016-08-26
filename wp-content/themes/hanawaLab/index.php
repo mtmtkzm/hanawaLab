c3f3502d90773bc919fc418614f4df8839dff2db
松本 和馬 <hello@mtmtkzm.com>
2016-08-27 00:53:02 +0900

    定型とcssの読み込み

--- wp-content/themes/hanawaLab/index.php
+++ wp-content/themes/hanawaLab/index.php
@@ -3,12 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>HanawaLab.</title>
-	<link rel="stylesheet" href="/css/style.css"> 
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
-<body>
-	<header>header</header>
-	<main>main</main>
-	<footer>footer</footer>
-	<script src="./js/app.js"></script>
+<body <?php body_class(); ?> >
+
+	<?php if(have_posts()): while(have_posts()): the_post(); ?>
+	<article <?php post_class(); ?>>
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	</article>
+
+	<?php endwhile; endif; ?>
+
+
 </body>
 </html>
