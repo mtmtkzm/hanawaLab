d4dab6bd9256abdd735d3c038840d3cdaf26b79d
松本 和馬 <hello@mtmtkzm.com>
2016-08-29 14:53:00 +0900

    no message

--- wp-content/themes/hanawaLab/index.php
+++ wp-content/themes/hanawaLab/index.php
@@ -6,14 +6,19 @@
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body <?php body_class(); ?> >
-	
+
 	<header class="header">
 		<div class="logo-path">
-			<a href="/"><h2><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt=""></h2></a>
+			<a href="/"><h2><img src="<?php bloginfo('template_directory'); ?>/images/common/logo.png" alt=""></h2></a>
 			<a href="/">Home</a>
 		</div>
 	</header>
-	<main class="main"></main>
+	<main class="main index">
+		<div class="lag-shadow">
+			<img src="<?php bloginfo('template_directory'); ?>/images/top/title.png">
+			<img class="shadow" src="<?php bloginfo('template_directory'); ?>/images/top/title-shadow.png">
+		</div>
+	</main>
 	<footer class="footer"></footer>
 
 	<?php if(have_posts()): while(have_posts()): the_post(); ?>
