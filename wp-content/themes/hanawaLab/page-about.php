f468a164381d5d98eef88bf90770ec7be34bd97a
松本 和馬 <hello@mtmtkzm.com>
2016-10-02 03:43:27 +0900

    固定ページを別で呼び出すように

--- wp-content/themes/hanawaLab/page-about.php
+++ wp-content/themes/hanawaLab/page-about.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
-	page.php
+	page.about
     <article <?php post_class(); ?> >
       <?php the_title(); ?>
       <?php the_content(); ?>
