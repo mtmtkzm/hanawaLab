a9247501c00585ae9918430969d8c8761d010ce0
松本 和馬 <hello@mtmtkzm.com>
2016-10-03 04:14:31 +0900

    contactのmailを追加

--- wp-content/themes/hanawaLab/page-contact.php
+++ wp-content/themes/hanawaLab/page-contact.php
@@ -2,7 +2,7 @@
   <div class="mv">
     <img src="<?php bloginfo('template_directory'); ?>/images/<?php the_title(); ?>/mv.png">
   </div>
-  <section class="schedule">
+  <section class="mail">
     <div class="h2">
       <div class="lag-shadow icon">
         <img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png">
@@ -11,7 +11,7 @@
     </div>
     <a href="mailto:hanawa@sda.nagoya-cu.ac.jp">hanawa@sda.nagoya-cu.ac.jp</a>
   </section>
-  <section class="access">
+  <section class="access full">
     <div class="h2">
       <div class="lag-shadow icon">
         <img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png">
@@ -21,12 +21,6 @@
     <div class="access-univ">
       <div class="univ-map" id="map_canvas"></div>
       <div class="univ-address">
-        名古屋市立大学芸術工学部 大学院芸術工学研究科<br>
-        〒464-0083 <br>
-        名古屋市千種区北千種2丁目1番10号 <br>
-        TEL 052-721-1225(代) <br>
-        FAX 052-721-3110
-
         名古屋市立大学芸術工学部 大学院芸術工学研究科<br>
         〒464-0083 <br>
         名古屋市千種区北千種2丁目1番10号 <br>
