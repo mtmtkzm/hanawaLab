c2ea92a1905829a9ec77f4b262bd65abff130777
松本 和馬 <hello@mtmtkzm.com>
2016-10-04 01:33:56 +0900

    address要素をチィか

--- wp-content/themes/hanawaLab/footer.php
+++ wp-content/themes/hanawaLab/footer.php
@@ -1,5 +1,6 @@
   </main>
   <footer id="footer">
+    <address>hanawa@sda.nagoya-cu.ac.jp</address>
     <small class="copyright">&copy; 2016 HanawaLab.</small>
   </footer>
   <?php wp_footer(); ?>
