966772755c59ed351c1c46ad135c4844cd1fe9d5
松本 和馬 <hello@mtmtkzm.com>
2016-10-03 23:33:03 +0900

    access−mapと住所のレイアウトを変更

--- wp-content/themes/hanawaLab/page-contact.php
+++ wp-content/themes/hanawaLab/page-contact.php
@@ -1,33 +1,30 @@
 <?php get_header(); ?>
   <div class="mv">
-    <img src="<?php bloginfo('template_directory'); ?>/images/<?php the_title(); ?>/mv.png">
+    <img src="<?php bloginfo('template_directory'); ?>/images/contact/mv.png">
   </div>
-  <section class="mail">
+  <section id="mail">
     <div class="h2">
-      <div class="lag-shadow icon">
-        <img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png">
-      </div>
+      <div class="lag-shadow icon"><img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png"></div>
       <h2>Get in touch</h2>
     </div>
     <a href="mailto:hanawa@sda.nagoya-cu.ac.jp">hanawa@sda.nagoya-cu.ac.jp</a>
   </section>
-  <section class="access full">
+  <section id="access">
     <div class="h2">
-      <div class="lag-shadow icon">
-        <img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png">
-      </div>
+      <div class="lag-shadow icon"><img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png"></div>
       <h2>Access to Univ.</h2>
     </div>
     <div class="access-univ">
-      <div class="univ-map" id="map_canvas"></div>
-      <div class="univ-address">
-        名古屋市立大学芸術工学部 大学院芸術工学研究科<br>
-        〒464-0083 <br>
-        名古屋市千種区北千種2丁目1番10号 <br>
-        TEL 052-721-1225(代) <br>
-        FAX 052-721-3110
+      <div class="univ-map">
+        <div id="map-canvas">ここに地図が表示されます</div>
+        <div class="univ-address">
+          名古屋市立大学芸術工学部 大学院芸術工学研究科<br>
+          〒464-0083 <br>
+          名古屋市千種区北千種2丁目1番10号 <br>
+          TEL 052-721-1225(代) <br>
+          FAX 052-721-3110
+        </div>
       </div>
     </div>
   </section>
-
 <?php get_footer(); ?>
