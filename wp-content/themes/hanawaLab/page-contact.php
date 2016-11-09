baa18aae8965fd2d1bf191d9dd6a027e58265f28
松本 和馬 <hello@mtmtkzm.com>
2016-11-09 18:21:21 +0900

    GoogleMap上にピンとしてロゴ（仮）を表示させた

--- wp-content/themes/hanawaLab/page-contact.php
+++ wp-content/themes/hanawaLab/page-contact.php
@@ -7,7 +7,10 @@
       <div class="lag-shadow icon"><img src="<?php bloginfo('template_directory'); ?>/images/top/i-whats.png"></div>
       <h2>Get in touch</h2>
     </div>
-    <a href="mailto:hanawa@sda.nagoya-cu.ac.jp">hanawa@sda.nagoya-cu.ac.jp</a>
+    <div>
+      <p>ご用のある方は、こちらまでメールをおねがいしますー</p>
+      <a href="mailto:hanawa@sda.nagoya-cu.ac.jp">hanawa@sda.nagoya-cu.ac.jp</a>
+    </div>
   </section>
   <section id="access">
     <div class="h2">
@@ -16,13 +19,17 @@
     </div>
     <div class="access-univ">
       <div class="univ-map">
-        <div id="map-canvas">ここに地図が表示されます</div>
+        <div id="map-canvas">GoogleMap[愛知県名古屋市千種区北千種2丁目1番10号]</div>
         <script src="https://maps.googleapis.com/maps/api/js"></script>
         <script>
-          var googlemap;
+          var map, marker;
+          var center = {
+            lat: 35.181691, // 緯度
+            lng: 136.947799  // 経度
+          };
           function initMap() {
-            googlemap = new google.maps.Map(document.getElementById('map-canvas'), {
-              center: {lat: 35.181691, lng: 136.947799},
+            map = new google.maps.Map(document.getElementById('map-canvas'), {
+              center: center,
               zoom: 16,
               scrollwheel: false,
               styles: [{
@@ -34,6 +41,16 @@
                   { lightness: 0 }
                 ]
               }]
+            });
+            marker = new google.maps.Marker({
+              position: center,
+              map: map,
+              icon: new google.maps.MarkerImage(
+                'http://localhost:8888/wp-content/themes/hanawaLab/images/common/logo.png',
+                new google.maps.Size(44,40), // size
+                new google.maps.Point(0,0),  // origin
+                new google.maps.Point(22,20) // anchor
+              )
             });
           }
           initMap();
