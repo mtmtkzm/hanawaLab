324f5f278152acbc5804d8be7e130744fef2f0a6
松本 和馬 <hello@mtmtkzm.com>
2017-02-27 01:48:40 +0900

    add separate introduction and achivement

---
+++ wp-content/themes/hanawaLab/archive-introduction.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="mv">
+  <img src="<?php bloginfo('template_directory'); ?>/images/research/mv.png">
+</div>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+<article <?php post_class(); ?>>
+  <a href="<?php the_permalink(); ?>">
+    <div class="post-content">
+      <h1 class="post-content-title"><?php the_title(); ?></h1>
+    </div>
+  </a>
+</article>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
