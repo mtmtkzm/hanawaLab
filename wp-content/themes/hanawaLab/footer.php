69a4394d4c39ab51c01e5bff71b4865c1d189f25
松本 和馬 <hello@mtmtkzm.com>
2017-03-19 12:32:29 +0900

    update footer

--- wp-content/themes/hanawaLab/footer.php
+++ wp-content/themes/hanawaLab/footer.php
@@ -7,8 +7,8 @@
     <section class="footer-link">
       <h3 class="footer-title">Links</h3>
       <ul class="social-link">
-        <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/ncu.jpg"></a></li>
-        <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/sda.jpg"></a></li>
+        <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/ncu.jpg"></a></li>
+        <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/sda.jpg"></a></li>
       </ul>
     </section>
     <small class="copyright">&copy; 2016 HanawaLab.</small>
