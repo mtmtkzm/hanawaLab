eeca25de360db7b0173343c219fd0a65af13dee3
松本 和馬 <hello@mtmtkzm.com>
2017-03-26 17:27:45 +0900

    update article page

--- wp-content/themes/hanawaLab/archive-introduction.php
+++ wp-content/themes/hanawaLab/archive-introduction.php
@@ -2,14 +2,16 @@
 <div class="mv">
   <img src="<?php bloginfo('template_directory'); ?>/images/research/mv.png">
 </div>
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
-<article <?php post_class(); ?>>
-  <a href="<?php the_permalink(); ?>">
-    <div class="post-content">
-      <h1 class="post-content-title"><?php the_title(); ?></h1>
-    </div>
-  </a>
-</article>
-<?php endwhile; endif; ?>
-
+<section id="introduction">
+  <?php if(have_posts()): while(have_posts()): the_post(); ?>
+  <article <?php post_class(); ?>>
+    <a href="<?php the_permalink(); ?>">
+      <div class="post-content">
+        <h1 class="post-content-title"><?php the_title(); ?></h1>
+      </div>
+    </a>
+  </article>
+  <?php endwhile; endif; ?>
+  
+</section>
 <?php get_footer(); ?>
