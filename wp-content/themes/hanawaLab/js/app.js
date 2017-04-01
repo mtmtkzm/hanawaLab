26b88f37969e2caf2a653274a8cfa157481cd555
松本 和馬 <hello@mtmtkzm.com>
2017-04-02 01:29:02 +0900

    update logo path

--- wp-content/themes/hanawaLab/js/app.js
+++ wp-content/themes/hanawaLab/js/app.js
@@ -1,3 +1,16 @@
-var currentPath = window.location.pathname.substr(1);
+
 var headerPath = document.querySelector('.js-directory');
-headerPath.innerHTML = currentPath;
+headerPath.innerHTML = formatPathname();
+headerPath.setAttribute('href', '/'+formatPathname());
+
+function formatPathname() {
+  var currentPath = window.location.pathname;
+  // 先頭の/を削除
+  currentPath = currentPath.substr(1);
+
+  var startPoint = currentPath.indexOf('/');
+  if (startPoint != -1) {
+    currentPath = currentPath.substr(0, startPoint);
+  }
+  return currentPath;
+}
