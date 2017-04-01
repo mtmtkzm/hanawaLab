8ac1b75c1f85a55159f05af0a1c182619ce3cbbc
松本 和馬 <hello@mtmtkzm.com>
2017-04-01 22:53:28 +0900

    add lecture page

--- wp-content/themes/hanawaLab/archive-lecture.php
+++ wp-content/themes/hanawaLab/archive-lecture.php
@@ -14,7 +14,17 @@
     <article <?php post_class(); ?>>
       <a href="<?php the_permalink(); ?>">
         <div class="post-content dropshadow">
-          <h1 class="post-content-title"><?php the_title(); ?></h1>
+          <div class="lecture-date">
+            <span class="lecture-day"><?php echo get_field('lecture_day'); ?></span>
+            <?php $periods = get_field('lecture_period'); if ($periods): ?>
+            <ul class="lecture-periods">
+              <?php foreach ($periods as $period) : ?>
+              <li><?php echo $period; ?></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+          <?php endif; ?>
+          <p class="lecture-name"><?php echo get_field('lecture_name'); ?></p>
         </div>
       </a>
     </article>
