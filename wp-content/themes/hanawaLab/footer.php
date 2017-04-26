43b2b47df6955a7ba74051dc41916e00e963d491
松本 和馬 <hello@mtmtkzm.com>
2017-04-26 20:38:26 +0900

    add footer-link

--- wp-content/themes/hanawaLab/footer.php
+++ wp-content/themes/hanawaLab/footer.php
@@ -2,16 +2,24 @@
   <footer id="footer">
     <section class="footer-address">
       <h3 class="footer-title">Contact</h3>
-      <address>hanawa@sda.nagoya-cu.ac.jp</address>
+      <address><a href="mailto:hanawa@sda.nagoya-cu.ac.jp">hanawa@sda.nagoya-cu.ac.jp</a></address>
     </section>
     <section class="footer-link">
       <h3 class="footer-title">Links</h3>
       <ul class="social-link">
-        <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/ncu.jpg"></a></li>
-        <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/sda.jpg"></a></li>
+        <li>
+          <a href="http://www.nagoya-cu.ac.jp/" target="_blank">
+            <img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/ncu.jpg">
+          </a>
+        </li>
+        <li>
+          <a href="http://www.nagoya-cu.ac.jp/sda/index.html" target="_blank">
+            <img src="<?php bloginfo('template_directory'); ?>/images/common/footer/banner/sda.jpg">
+          </a>
+        </li>
       </ul>
     </section>
-    <small class="copyright">&copy; 2016 HanawaLab.</small>
+    <small class="copyright">&copy; 2017 HanawaLab.</small>
   </footer>
   <script src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
   <?php wp_footer(); ?>
